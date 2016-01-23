28a58fd9dd130c18514b2e57854083d37693377a
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2016-01-23 10:08:47 +0800

    added the high level overview and the route file for home page

---
+++ public/app/app.routes.js
@@ -0,0 +1,15 @@
+angular.module('app.routes', ['ngRoute'])
+
+.config(function($routeProvider, $locationProvider) {
+
+	$routeProvider
+
+		// route for the home page
+		.when('/', {
+			templateUrl : 'app/views/pages/home.html'
+		})
+
+    // get rid of the hash in the URL 
+    $locationProvider.html5Mode(true);
+
+});
